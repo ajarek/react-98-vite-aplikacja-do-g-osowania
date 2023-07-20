ec50319f1a48f79e10ed465f12f9865998f9dcd9
Ajarek <ajarek@poczta.onet.pl>
2023-07-20 23:03:05 +0200

    added responsiveness

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,41 +1,62 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-
-
+import Hamburger from 'hamburger-react'
 import './Nav.css'
 
 const Nav = () => {
-  return (
-    <>
-      <nav className='nav'>
-        <div className='logo'>
-          <Link
-            className='link'
-            to={'/'}
-          >
-            <div className="img">
-            <img src="./orzeł.png" alt="" />
-            </div>
-            <h1>Wybory 2023</h1>
-          </Link>
-        </div>
+  const [isOpen, setOpen] = useState(false)
 
-        <ul className='wrapper'>
-          <Link
-            className='link'
-            to={'/'}
-          >
-           Panel Głosowania
-          </Link>
-          <Link
-            className='link'
-            to={'/wyniki'}
-          >
-           Wyniki
-          </Link>
-          
-        </ul>
-      </nav>
-    </>
+  return (
+    <nav className='nav'>
+      <div className='logo'>
+        <Link
+          className='link link-logo'
+          to={'/'}
+        >
+          <div className='img'>
+            <img
+              src='./orzeł.png'
+              alt=''
+            />
+          </div>
+          <h1>Wybory 2023</h1>
+        </Link>
+      </div>
+      <ul className={!isOpen ? 'wrapper' : 'wrapper navbar-none'}>
+        <Link
+          className='link link-text'
+          to={'/'}
+        >
+          Panel Głosowania
+        </Link>
+
+        <Link
+          className='link link-text'
+          to={'/wyniki'}
+        >
+          Wyniki
+        </Link>
+      </ul>
+      <div className='hamburger'>
+        <Hamburger
+          size={30}
+          duration={0.3}
+          distance='md'
+          color={isOpen ? '#ff3f34' : '#1e272e'}
+          easing='ease-in'
+          rounded
+          label='Show menu'
+          onToggle={(toggled) => {
+            setOpen(true)
+            if (toggled) {
+              // open a menu
+            } else {
+              setOpen(false)
+            }
+          }}
+        />
+      </div>
+    </nav>
   )
 }
 
